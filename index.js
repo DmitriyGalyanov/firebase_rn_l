a431937a12c0f1411dd4ea264abe9d3c7a529992
DmitriyGalyanov <dimagalyanov@gmail.com>
2020-11-08 01:36:27 +0300

    init
    receives Firebase Cloud Messaging notifications;
    
    alerts notification data (app in both foreground and background & quit states);
    
    retrieves and logs FCM token

--- index.js
+++ index.js
@@ -3,7 +3,24 @@
  */
 
 import {AppRegistry} from 'react-native';
+
+import messaging from '@react-native-firebase/messaging';
+
 import App from './App';
 import {name as appName} from './app.json';
+
+import iid from '@react-native-firebase/iid';
+
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+	console.log('Message handled in the background!', remoteMessage);
+	console.log('And I\'m not sure what am I supposed to do about it')
+});
+
+async function getInstanceId() {
+	const token = await iid().getToken();
+	console.log('FCM token', token);
+}
+
+getInstanceId();
 
 AppRegistry.registerComponent(appName, () => App);
