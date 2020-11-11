f6fb404b8bfae3627ce1783f395d8e4ce13db503
DmitriyGalyanov <dimagalyanov@gmail.com>
2020-11-11 14:50:17 +0300

    0.5.6
    set remoteConfig configSettings

--- App.js
+++ App.js
@@ -21,36 +21,61 @@ import messaging from '@react-native-firebase/messaging';
 import GameScreen from 'screens/GameScreen';
 
 
+function fetchRemoteConfig() {
+
+	remoteConfig()
+	.setDefaults({
+		'test_3': 'platform__unknown',
+	})
+	.then(() => {
+		return remoteConfig().setConfigSettings({
+			minimumFetchIntervalMillis: 100,
+		})
+	})
+	.then(() => {
+		// const test_3 = remoteConfig().getValue('test_3')
+		// console.log(test_3, 'test3')
+		return remoteConfig().fetchAndActivate()
+	})
+	.then(() => {
+		return remoteConfig().ensureInitialized()
+	})
+	.then(fetchedRemotely => {
+		if (fetchedRemotely) {
+			console.log('Configs were retrieved from the backend and activated.');
+
+		} else {
+			console.log(
+				'No configs were fetched from the backend, and the local configs were already activated',
+			);
+		}
+	});
+};
+
 const App: () => React$Node = () => {
 
 	useEffect(() => {
-		remoteConfig()
-			.setDefaults({
-				test_condition3: 'not_fetched',
-			})
-			// .then(() => {
-			// 	console.log('default remote config values set')
-			// })
-			.then(() => remoteConfig().fetchAndActivate())
-			// .then(fetched => {
-			// 	if (fetched) {
-			// 		console.log('configs retieved and activated');
-			// 	} else {
-			// 		console.log('no configs were fetched')
-			// 	}
-			// })
-			// .then(() => console.log(remoteConfig().getAll(), 'effect'))
+		//await it?
+		fetchRemoteConfig();
+	}, [fetchRemoteConfig]);
 
-	}, []);
-
-	// console.log(remoteConfig().getAll(), 'render')
-	
 	useEffect(() => {
 		const unsubscribe = messaging().onMessage(async remoteMessage => {
 			Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
 		});
 
 		return unsubscribe;
+	}, []);
+
+	useEffect(() => {
+
+	});
+
+	useEffect(() => {
+		// TO REMOVE [COMMENT]
+		// when we have link (from remote config) we display webView
+		// otherwise display game part
+		// TO REMOVE [COMMENT]
 	}, []);
 
 	return (
