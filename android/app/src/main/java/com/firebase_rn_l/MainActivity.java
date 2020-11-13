539989aae4c26629364139f4cfb365504a750735
DmitriyGalyanov <dimagalyanov@gmail.com>
2020-11-13 14:00:48 +0300

    0.7.1
    enable facebook deferred app links:

--- android/app/src/main/java/com/firebase_rn_l/MainActivity.java
+++ android/app/src/main/java/com/firebase_rn_l/MainActivity.java
@@ -3,7 +3,31 @@ package com.test.test;
 
 import com.facebook.react.ReactActivity;
 
+import com.facebook.applinks.AppLinkData;
+import android.os.Bundle; // required for onCreate parameter
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+
 public class MainActivity extends ReactActivity {
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    AppLinkData.fetchDeferredAppLinkData(this, getString(R.string.facebook_app_id),
+      new AppLinkData.CompletionHandler() {
+          @Override
+          public void onDeferredAppLinkDataFetched(AppLinkData appLinkData) {
+              Log.d("SplashActivity", "appLinkData: " + appLinkData);
+          }
+      }
+    );
+
+    Intent intent = getIntent();
+    String action = intent.getAction();
+    Uri data = intent.getData();// URI data = intent.getData();
+    Log.d("SplashActivity", "data: " + data);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
